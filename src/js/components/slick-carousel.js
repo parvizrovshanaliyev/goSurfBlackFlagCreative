0563e2bd034eb750fdf8a94ab9fea43123f241b9
parvizrovshanaliyev <parvizra@code.edu.az>
2019-11-26 16:24:34 +0400

    slider ok js loading

--- src/js/components/slick-carousel.js
+++ src/js/components/slick-carousel.js
@@ -4,6 +4,12 @@ $(function () {
         infinite:true,
         fade: true,
         prevArrow:'<img class="slider-arrows slider-arrows_left" src="../img/arrows-left.svg" alt=""></img>',
-        nextArrow:'<img class="slider-arrows slider-arrows_right" src="../img/arrows-right.svg" alt=""></img>'
+        nextArrow:'<img class="slider-arrows slider-arrows_right" src="../img/arrows-right.svg" alt=""></img>',
+        asNavFor: '.slider-dots',
+    });
+    $('.slider-dots').slick({
+        slidesToShow:4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
     });
 });
