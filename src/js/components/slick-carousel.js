767e00c267068adb34ec632d1a1b8bb623c1009a
parvizrovshanaliyev <parvizra@code.edu.az>
2019-11-28 00:28:16 +0400

    en son sliderda qalmisdim

--- src/js/components/slick-carousel.js
+++ src/js/components/slick-carousel.js
@@ -12,4 +12,10 @@ $(function () {
         slidesToScroll: 4,
         asNavFor: '.header__slider',
     });
+    $('.surf-slider').slick({
+        slidesToShow:4,
+        slidesToScroll: 1,
+        prevArrow:'<img class="slider-arrows slider-arrows_left" src="../img/arrows-left.svg" alt=""></img>',
+        nextArrow:'<img class="slider-arrows slider-arrows_right" src="../img/arrows-right.svg" alt=""></img>',
+    });
 });
