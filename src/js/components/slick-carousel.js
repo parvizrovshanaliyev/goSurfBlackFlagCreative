fb9a54f53e2d754dd01b3d9618e9afcdf5dbad46
parvizrovshanaliyev <parvizra@code.edu.az>
2019-11-28 23:32:26 +0400

    sliderdotsheader

--- src/js/components/slick-carousel.js
+++ src/js/components/slick-carousel.js
@@ -5,9 +5,9 @@ $(function () {
         fade: true,
         prevArrow:'<img class="slider-arrows slider-arrows_left" src="../img/arrows-left.svg" alt=""></img>',
         nextArrow:'<img class="slider-arrows slider-arrows_right" src="../img/arrows-right.svg" alt=""></img>',
-        asNavFor: '.slider-dots',
+        asNavFor: '.slider-dotsheader',
     });
-    $('.slider-dots').slick({
+    $('.slider-dotsheader').slick({
         slidesToShow:4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
