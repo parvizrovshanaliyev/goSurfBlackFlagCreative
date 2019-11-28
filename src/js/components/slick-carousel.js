a1811dd002d4ca4d3bb08f4e31c1d679a14574c0
parvizrovshanaliyev <parvizra@code.edu.az>
2019-11-28 22:54:35 +0400

    ok

--- src/js/components/slick-carousel.js
+++ src/js/components/slick-carousel.js
@@ -21,29 +21,29 @@ $(function () {
         nextArrow:'<img class="slider-arrows slider-arrows_right" src="../img/arrows-right.svg" alt=""></img>',
         pauseOnHover: true,
 
-        responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 5,
-                slidesToScroll: 5,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        }
+        // responsive: [
+        // {
+        //     breakpoint: 1024,
+        //     settings: {
+        //         slidesToShow: 5,
+        //         slidesToScroll: 5,
+        //     }
+        // },
+        // {
+        //     breakpoint: 600,
+        //     settings: {
+        //         slidesToShow: 3,
+        //         slidesToScroll: 3
+        //     }
+        // },
+        // {
+        //     breakpoint: 480,
+        //     settings: {
+        //         slidesToShow: 2,
+        //         slidesToScroll: 2
+        //     }
+        // }
 
-        ]
+        // ]
     });
 });
